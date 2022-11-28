995f4531b0efeb1437145dfb6e35ca62db82ca53
NouXXXX <leedh50778037@gmail.com>
2022-11-28 11:47:37 +0900

    Add: front design

--- src/componets/AxEx.js
+++ src/componets/AxEx.js
@@ -1,38 +1,48 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios'
+import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
+import "./style.css";
+
 const AxEx = (props) => {
-    const lineValue = useSelector((state) => state); // form: hook
-    const url = "http://localhost:8080/cham_list";
-    const [data,setData] = useState([]);
-    useEffect(() => {
-        axios.get(url)
-        .then(function(response) {
-            setData(response.data);
-            console.log("get seccess!!!");
-//            console.log(data);
-        }).catch(function(error) {
-            console.log("faild");
-            });
-    },[]);
+  const lineValue = useSelector((state) => state); // form: hook
+  const url = "http://10.150.150.2:5000/cham_list";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(function (response) {
+        setData(response.data);
+        console.log("get seccess!!!");
+        //            console.log(data);
+      })
+      .catch(function (error) {
+        console.log("faild");
+      });
+  }, []);
 
-    let rt =[];
+  let rt = [];
 
-    data.map((pro) => {
-        if ( pro.line_name == lineValue ) { // pro.line_name == props.line
-            rt.push(
-                    <div id="cham-list">
-                        <span id="cham-name">
-                            <img src={`https://opgg-static.akamaized.net/meta/images/lol/champion/${pro.cham_id}.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_64&v=1668401198263`} alt="champion-img" />
-                            {pro.cham_name}
-                        </span>
-                        <span id="cham-tier">{pro.tier}</span>
-                        <span id="cham-win">{pro.win_rate}%</span>
-                        <span id="cham-ban">{pro.ban_rate}%</span>
-                        <span id="cham-pick">{pro.pick_rate}%</span>
-                    </div>
-                    );
-        }});
-    return rt;
-}
+  data.map((pro) => {
+    if (pro.line_name == lineValue) {
+      // pro.line_name == props.line
+      rt.push(
+        <div id="cham-list">
+          <span id="cham-img">
+            <img
+              src={`https://opgg-static.akamaized.net/meta/images/lol/champion/${pro.cham_id}.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_64&v=1668401198263`}
+              alt="champion-img"
+            />
+          </span>
+          <span id="cham-name">{pro.cham_name}</span>
+          <span id="cham-tier">{pro.tier}티어</span>
+          <span id="cham-win">승률 : {pro.win_rate}%</span>
+          <span id="cham-ban">밴률 : {pro.ban_rate}%</span>
+          <span id="cham-pick">픽률 : {pro.pick_rate}%</span>
+        </div>
+      );
+    }
+  });
+  return rt;
+};
 export default AxEx;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import Header from './componets/Header';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./componets/Header";
 import AxEx from "./componets/AxEx";
 import MainContents from "./componets/MainContents";
 import { StrictMode } from "react";
@@ -15,29 +15,24 @@ const root = createRoot(rootElement);
 const initialValue = "탑";
 
 function reducer(state = initialValue, action) {
-    console.log(action.type);
-    if (action.type === "top") return state="탑";
-    else if (action.type === "jungle") return state="정글";
-    else if (action.type === "mid") return state="미드";
-    else if (action.type === "adc") return state="원딜";
-    else if (action.type === "support") return state="서폿";
-    else return state;
+  console.log(action.type);
+  if (action.type === "top") return (state = "탑");
+  else if (action.type === "jungle") return (state = "정글");
+  else if (action.type === "mid") return (state = "미드");
+  else if (action.type === "adc") return (state = "원딜");
+  else if (action.type === "support") return (state = "서폿");
+  else return state;
 }
 
 let store = createStore(reducer);
 
 root.render(
-        <StrictMode>
-            <Provider store={store}>
-                <Header />
-                <MainContents data=<AxEx line="미드" /> />
-            </Provider>
-<<<<<<< HEAD
-        </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <Header />
+      <MainContents data=<AxEx line="미드" /> />
+    </Provider>
+  </StrictMode>
 );
-=======
-            </StrictMode>
-            );
->>>>>>> 8a4d5aeebb3756f3f616e0655e60172fd20c501a
 
 reportWebVitals();
